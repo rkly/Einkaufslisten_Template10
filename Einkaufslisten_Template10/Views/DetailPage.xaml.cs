3fff04f3be1fe0bacc3851516ac848fbd7075199
romankl1m <romnklymko@gmail.com>
2017-05-28 17:12:10 +0200

    Projektdateien hinzufügen.

---
+++ Einkaufslisten_Template10/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Einkaufslisten_Template10.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Einkaufslisten_Template10.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
---
+++ Einkaufslisten_Template10/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Einkaufslisten_Template10.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace Einkaufslisten_Template10.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
